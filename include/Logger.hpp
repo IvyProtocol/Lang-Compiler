5d0d5e08c9006d89403dc39c1296656d571e89c6
Vyle <121424993+IvyProtocol@users.noreply.github.com>
2026-06-07 19:11:44 +0600

    Refactor Logger: Remove log_err and add ConsoleColor
    Removed log_err functions and added ConsoleColor namespace.

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -13,13 +13,16 @@ void log_mov(std::string_view msg, size_t line, size_t col) {
   log_mov(msg, t.line, t.column);
 }
 
-void log_err(std::string_view msg, size_t line, size_t col) {
-  std::println("[ERR]: {} at Line {}, Column {}", msg, line, col);
-}
-[[maybe_unused]] void log_err(std::string_view msg, const Token &t) {
-  log_err(msg, t.line, t.column);
-}
-
 } // namespace
 
+namespace ConsoleColor {
+inline const std::string RED = "\033[1;31m";
+inline const std::string GREEN = "\033[1;32m";
+inline const std::string YELLOW = "\033[1;33m";
+inline const std::string RESET = "\033[0m";
+
+inline const std::string BOLD_RED = "\033[1;31m";
+inline const std::string BOLD_GREEN = "\033[1;32m";
+inline const std::string BOLD_YELLOW = "\033[1;33m";
+} // namespace ConsoleColor
 #endif
