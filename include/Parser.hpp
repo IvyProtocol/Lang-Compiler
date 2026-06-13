9f931bdf68c343569aabdd02a5923e31390ed401
Vyle <121424993+IvyProtocol@users.noreply.github.com>
2026-06-13 10:25:00 +0600

    Enhance ASTNode options and parser functionality
    Added new options for ASTNode structures and improved parser methods for better handling of parameters and conditions.

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -32,10 +32,12 @@ enum class ParseError {
   MissingClosingBrace,
   MissingArrow,
   MissingColon,
+  MissingComma,
   MalformedParameters,
   MalformedRangeLiteral,
   MalformedMemberAccess,
   MalformedLoopCondition,
+  ExpectedBraceOrColon,
   InvalidImportPath,
   InvalidExpression,
   InvalidDeclarationTarget,
@@ -68,13 +70,23 @@ struct Parser {
     bool allow_assignment = false;
     bool allow_ranges = false;
     bool fatal_on_assign = false;
-
+    bool allow_void_assignment = false;
     char pad[6]{};
   };
 
   struct LoopOptsASTNode {
     bool allow_C_style = false;
     bool allow_Loop_Range_style = false;
+    bool allow_body = false;
+  };
+
+  struct alignas(8) ParsedIfHeader {
+    std::unique_ptr<ASTNode> initializer{nullptr};
+    std::unique_ptr<ASTNode> condition{nullptr};
+    bool has_paren{false};
+    bool enforce_brace{false};
+
+    std::byte _padding[6]{};
   };
 
   std::vector<Token> tokens;
@@ -86,13 +98,13 @@ struct Parser {
   Parser(std::vector<Token> tokens_list, std::string_view file_path,
          std::vector<std::string> lines);
 
-  bool is_at_end() const {
+  [[nodiscard]]bool is_at_end() const {
     return current >= tokens.size() ||
            tokens[current].type == TokenType::END_OF_FILE;
   }
 
-  const Token &peek() const;
-  bool check(TokenType type) const {
+  [[nodiscard]]const Token &peek() const;
+  [[nodiscard]]bool check(TokenType type) const {
     if (is_at_end())
       return false;
     return peek().type == type;
@@ -104,7 +116,7 @@ struct Parser {
     return tokens[current - 1];
   }
 
-  Token peek_next() const {
+  [[nodiscard]]Token peek_next() const {
     if (current + 1 >= tokens.size())
       return Token{TokenType::END_OF_FILE, "", 0, 0};
     return tokens[current + 1];
@@ -113,18 +125,20 @@ struct Parser {
   std::expected<Token, ParseError> consume_token(TokenType type,
                                                  std::string_view msg);
   bool consume(TokenType type, std::string_view msg);
+
   bool parse_parameter_list(std::vector<ParameterASTNode> &params,
                             const ParameterOptsASTNode &opts,
                             TokenType term_type);
 
   bool parse_loopParam_list(const LoopOptsASTNode &cond,
                             std::vector<LoopConditionASTNode> &c_block);
+
   bool parser_for_variable_group(std::vector<ParameterASTNode> &params);
   bool parser_function_parameter_group(std::vector<ParameterASTNode> &params);
   bool parser_variable_parameter_group(std::vector<ParameterASTNode> &params);
   bool parser_for_condition_group(std::vector<LoopConditionASTNode> &params);
-  bool is_statement_start(TokenType type);
-  ParserRule get_rule(TokenType type);
+  static bool is_statement_start(TokenType type);
+  static ParserRule get_rule(TokenType type);
   void report_error(const Token &tok, std::string_view msg);
   void synchronize();
 
@@ -155,19 +169,21 @@ struct Parser {
   std::expected<std::unique_ptr<ASTNode>, ParseError>
   parse_index(std::unique_ptr<ASTNode> left);
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_block();
+
+  std::expected<ParsedIfHeader, ParseError> parse_if_header();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_paren();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_import_statement();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_paren_expression();
-  std::expected<std::unique_ptr<ASTNode>, ParseError> parse_if_body();
+  std::expected<std::unique_ptr<ASTNode>, ParseError> parse_if_body(const ParsedIfHeader& header);
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_if_statement();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_for_statement();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_return_statement();
+  std::expected<std::unique_ptr<ASTNode>, ParseError> parse_join();
   std::expected<std::unique_ptr<ASTNode>, ParseError>
   parse_function_statement();
 
   std::expected<std::unique_ptr<ASTNode>, ParseError>
   parse_namespace_statement();
-  std::expected<std::unique_ptr<ASTNode>, ParseError> parse_switch_statement();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_statement();
   std::expected<std::unique_ptr<ASTNode>, ParseError> parse_primary();
 };
