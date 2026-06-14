129fa30b247e20e09b1babce21489e936141947e
Vyle <121424993+IvyProtocol@users.noreply.github.com>
2026-06-14 21:52:08 +0600

    Change string declarations to constexpr in Logger.hpp

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -16,13 +16,13 @@ void log_mov(std::string_view msg, size_t line, size_t col) {
 } // namespace
 
 namespace ConsoleColor {
-inline const std::string RED = "\033[1;31m";
-inline const std::string GREEN = "\033[1;32m";
-inline const std::string YELLOW = "\033[1;33m";
-inline const std::string RESET = "\033[0m";
+inline constexpr std::string RED = "\033[1;31m";
+inline constexpr std::string GREEN = "\033[1;32m";
+inline constexpr std::string YELLOW = "\033[1;33m";
+inline constexpr std::string RESET = "\033[0m";
 
-inline const std::string BOLD_RED = "\033[1;31m";
-inline const std::string BOLD_GREEN = "\033[1;32m";
-inline const std::string BOLD_YELLOW = "\033[1;33m";
+inline constexpr std::string BOLD_RED = "\033[1;31m";
+inline constexpr std::string BOLD_GREEN = "\033[1;32m";
+inline constexpr std::string BOLD_YELLOW = "\033[1;33m";
 } // namespace ConsoleColor
 #endif
